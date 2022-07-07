002fcb48e74241896eb675fc8e7fa18228b184d7
Ben McAllister <bmcallister@xpansiv.com>
2022-07-06 17:51:47 -0700

    added sass configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,32 @@
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-
+const sass = require("sass");
+const path = require("node:path");
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addTemplateFormats("scss");
+
+    // Creates the extension for use
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css", // optional, default: "html"
+
+        // `compile` is called once per .scss file in the input directory
+        compile: async function(inputContent, inputPath) {
+            let parsed = path.parse(inputPath);
+            let result = sass.compileString(inputContent, {
+                loadPaths: [
+                    parsed.dir || ".",
+                    this.config.dir.includes
+                ]
+            });
+
+            // This is the render function, `data` is the full data cascade
+            return async (data) => {
+                return result.css;
+            };
+        }
+    });
 
     // eleventyConfig.addPassthroughCopy("**/*.jpg");
     eleventyConfig.addPassthroughCopy("js");
