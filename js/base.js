838025782f5ee3f6c47ae72dc4ad892db06074c4
Ben McAllister <bmcallister@xpansiv.com>
2022-07-09 16:58:48 -0700

    nav working with submenus, with test html

--- js/base.js
+++ js/base.js
@@ -3,5 +3,22 @@ $(document).ready(function() {
         $(".block-nav_toggle").toggleClass('toggled');
         $(".block-nav_home").toggleClass('toggled');
         $(".block-nav_options").toggleClass('toggled');
-    })
+        //clear scrim and details
+        if(!$(".block-nav").hasClass("toggled")){
+            $(".block-nav_detail").hide();
+        }
+    });
+
+    $(".block-nav_option").click(function () {
+        // if this is toggled, silently fail
+        if($(this).not("toggled")){
+            $(".block-nav_detail").hide();
+            $(".block-nav_option.toggled").toggleClass("toggled");
+            $(this).toggleClass("toggled");
+            var selected_tag = $(this).attr("data");
+            $(".block-nav_detail[data=" + selected_tag + "]").show();
+            $(".block-nav_detail[data=" + selected_tag + "]").scrollTop(0);
+        }
+    });
 });
+
