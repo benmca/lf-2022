1364980bb269b865dab7cc43a27d224b3b2bb325
Ben McAllister <ben@listenfaster.com>
2023-06-13 11:00:46 -0700

    sync scripts

---
+++ sync-localtoserver.sh
@@ -0,0 +1,4 @@
+rsync -avz ./assets/ benmca@listenfaster.com:listenfaster.com/main/assets
+rsync -avz ./img/  benmca@listenfaster.com:listenfaster.com/main/img
+rsync -avz ./snd/ benmca@listenfaster.com:listenfaster.com/main/snd
+
---
+++ sync-servertolocal.sh
@@ -0,0 +1,4 @@
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/assets .
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/img .
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/snd .
+
