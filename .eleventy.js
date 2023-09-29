85c42e6f1a7a6d16e356105342fea9123da5ebfd
Ben McAllister <ben@listenfaster.com>
2023-09-29 13:04:36 -0700

    ripped out the javascript and moved to pure css for nav

--- .eleventy.js
+++ .eleventy.js
@@ -95,6 +95,7 @@ module.exports = function (eleventyConfig) {
         linkify: false
     });
     eleventyConfig.setLibrary("md", markdownLibrary);
+    eleventyConfig.addWatchTarget("./_includes/_styles");
 
     return {
         // Control which files Eleventy will process
