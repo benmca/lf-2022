dd2dafefe2973c2ec48b3f42140f6203cff4e32f
Ben McAllister <ben@listenfaster.com>
2024-06-25 12:51:14 -0700

    posts from moving time

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ rm -rf ./_site/
 ./make_thumbs.sh
 npx @11ty/eleventy
 rsync -avz --delete ./_site/ benmca@listenfaster.com:listenfaster.com/main
+rsync -avz --delete ./snd/1min/*.mp3 ~/Dropbox/Portfolio/minutes/
+
