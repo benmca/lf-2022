2e550069d6837ac225f40cc1b083ecc035c74f85
Ben McAllister <ben@listenfaster.com>
2026-09-24 09:20:02 -0700

    fixed ancient mistake

--- sync-servertolocal.sh
+++ sync-servertolocal.sh
@@ -1,4 +1,3 @@
-rsync -avz ./assets/ benmca@listenfaster.com:listenfaster.com/main/assets
-rsync -avz ./img/  benmca@listenfaster.com:listenfaster.com/main/img
-rsync -avz ./snd/ benmca@listenfaster.com:listenfaster.com/main/snd
-
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/assets ./assets/ 
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/img ./img/  
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/snd ./snd/ 
