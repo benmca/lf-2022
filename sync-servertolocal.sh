f69fdd69d13efb11098c0fe2930734b67ca8530c
Ben McAllister <ben@listenfaster.com>
2026-09-24 13:26:32 -0700

    another bug

--- sync-servertolocal.sh
+++ sync-servertolocal.sh
@@ -1,3 +1,3 @@
-rsync -avz benmca@listenfaster.com:listenfaster.com/main/assets ./assets/ 
-rsync -avz benmca@listenfaster.com:listenfaster.com/main/img ./img/  
-rsync -avz benmca@listenfaster.com:listenfaster.com/main/snd ./snd/ 
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/assets . 
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/img .  
+rsync -avz benmca@listenfaster.com:listenfaster.com/main/snd . 
